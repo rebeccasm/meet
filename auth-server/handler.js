b7e50cb90f1b6346815b8709bf0601e5966ebebe
rebeccasm <130580597+rebeccasm@users.noreply.github.com>
2024-12-16 15:30:59 +0100

    Update

--- auth-server/handler.js
+++ auth-server/handler.js
@@ -84,37 +84,45 @@ module.exports.getAccessToken = async (event) => {
 // };
 
 module.exports.getCalendarEvents = async (event) => {
- const access_token = event.pathParameters.access_token;
-// code to youse access token
+
+  const access_token = decodeURIComponent(
+    `${event.pathParameters.access_token}`
+  );
   oAuth2Client.setCredentials({ access_token });
 
+
   return new Promise((resolve, reject) => {
-    // logic to fetch calendar events
     calendar.events.list(
       {
         calendarId: CALENDAR_ID,
         auth: oAuth2Client,
         timeMin: new Date().toISOString(),
         singleEvents: true,
-        orderBy: 'startTime',
+        orderBy: "startTime",
       },
       (error, response) => {
         if (error) {
-          reject({
-            // errors if the promise is rejected
-            statusCode: 500,
-            body: JSON.stringify({ error: 'Failed to fetch events' }),
-            headers: { "Access-Control-Allow-Origin": "*" },
-          });
+          reject(error);
         } else {
-          resolve({
-            // the resolved promise
-            statusCode: 200,
-            body: JSON.stringify({ events: response.data.items }),
-            headers: { "Access-Control-Allow-Origin": "*" },
-          });
+          resolve(response);
         }
       }
     );
-  });
+  })
+    .then((results) => {
+      return {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        },
+        body: JSON.stringify({ events: results.data.items }),
+      };
+    })
+    .catch((error) => {
+      return {
+        statusCode: 500,
+        body: JSON.stringify(error),
+      };
+    });
 };
